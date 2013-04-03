8036dc19126e9e980c2662d5d46e17a9188f1ce0
Hiroaki Nakamura <hnakamur@gmail.com>
2013-04-03 14:29:53 +0900

    Change path prefix from /tmp/chef-solo to /etc/chef in solo.rb

--- solo.rb
+++ solo.rb
@@ -1,6 +1,6 @@
-file_cache_path           "/tmp/chef-solo"
-data_bag_path             "/tmp/chef-solo/data_bags"
-encrypted_data_bag_secret "/tmp/chef-solo/data_bag_key"
-cookbook_path             [ "/tmp/chef-solo/site-cookbooks",
-                            "/tmp/chef-solo/cookbooks" ]
-role_path                 "/tmp/chef-solo/roles"
+file_cache_path           "/etc/chef"
+data_bag_path             "/etc/chef/data_bags"
+encrypted_data_bag_secret "/etc/chef/data_bag_key"
+cookbook_path             [ "/etc/chef/site-cookbooks",
+                            "/etc/chef/vendor/cookbooks" ]
+role_path                 "/etc/chef/roles"
