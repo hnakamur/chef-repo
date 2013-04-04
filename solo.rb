0a479dbf34162d0368c46a4ce790b588e847c6e0
Hiroaki Nakamura <hnakamur@gmail.com>
2013-04-04 18:37:15 +0900

    Removed site-cookbooks directory.

--- solo.rb
+++ solo.rb
@@ -1,6 +1,6 @@
 file_cache_path           "/etc/chef"
 data_bag_path             "/etc/chef/data_bags"
 encrypted_data_bag_secret "/etc/chef/data_bag_key"
-cookbook_path             [ "/etc/chef/site-cookbooks",
+cookbook_path             [ "/etc/chef/cookbooks",
                             "/etc/chef/vendor/cookbooks" ]
 role_path                 "/etc/chef/roles"
